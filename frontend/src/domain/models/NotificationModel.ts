5fd5cd91e43fef0427e8040537c0f6171d1a8bf4
Nguyen Vo Hoang Son <sonmessia123@gmail.com>
2025-11-24 20:10:05 +0700

    Revert "[FRONTEND] Add Dashboard to display OSM and traffic flow"

--- frontend/src/domain/models/NotificationModel.ts
+++
@@ -1,6 +0,0 @@
-export interface NotificationModel {
-  id: string;
-  type: "error" | "warning" | "info" | "success";
-  message: string;
-  timestamp: Date;
-}
--- frontend/src/domain/models/SenSorModel.ts
+++
@@ -1,9 +0,0 @@
-import type { AirQualityObservedModel } from "./AirQualityObservedModel";
-import type { WaterQualityObservedModel } from "./WaterQualityObservedModel";
-
-export type SensorModel = AirQualityObservedModel | WaterQualityObservedModel;
-
-export enum SensorType {
-  AIR_QUALITY = "AirQualityObserved",
-  WATER_QUALITY = "WaterQualityObserved",
-}
