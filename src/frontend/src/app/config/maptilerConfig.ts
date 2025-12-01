ce1bc0ca78e052a7d1a504b95d42df07fa557524
TriHuynh <huynhminhtri1109@gmail.com>
2025-12-01 11:01:43 +0700

    refactor: remove ChatInput component and its associated styles.

--- src/frontend/src/app/config/maptilerConfig.ts
+++
@@ -1,1 +0,0 @@
-export const MAPTILER_API_KEY = import.meta.env.VITE_MAPTILER_API_KEY as string;
--- src/frontend/src/presentation/components/feature/devices/index.ts
+++
@@ -1,3 +0,0 @@
-export { DeviceCard } from "./DeviceCard";
-export { DeviceWizard } from "./DeviceWizard";
-export { FilterBar } from "./FilterBar";
